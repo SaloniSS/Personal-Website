ae370078a0f526763cbbd747912158a6c2154d9e
SaloniSS <sxs170015@utdallas.edu>
2020-07-24 01:04:59 -0500

    Remove unneeded stuff

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -4,31 +4,7 @@ const {
   getProjects,
   addProject,
 } = require("../controllers/project_controller");
-const {
-  getWinningProjects,
-  addWinningProject,
-} = require("../controllers/winning_project_controller");
-const {
-  getLongProjects,
-  addLongProject,
-} = require("../controllers/long_project_controller");
-const {
-  getFeaturedProjects,
-  addFeaturedProject,
-} = require("../controllers/featured_project_controller");
 
 router.route("/").get(getProjects).post(addProject);
-
-router
-  .route("/winning-projects")
-  .get(getWinningProjects)
-  .post(addWinningProject);
-
-router.route("/long-projects").get(getLongProjects).post(addLongProject);
-
-router
-  .route("/featured-projects")
-  .get(getFeaturedProjects)
-  .post(addFeaturedProject);
 
 module.exports = router;
