eae3dad57e54b8a9016484ba040193de9c3929d9
SaloniSS <sxs170015@utdallas.edu>
2020-07-23 23:49:48 -0500

    Testing script

--- src/scripts/ActivitiesSection.js
+++ src/scripts/ActivitiesSection.js
@@ -1,74 +1,75 @@
 // /src/components/AchievementsSection.js
-import React from 'react';
+import React from "react";
 import {
-    Container,
-    Row,
-    Col,
-    Card,
-    CardText,
-    CardBody,
-    CardTitle } from 'reactstrap';
-import '../styles/ActivitiesSection.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
+  Container,
+  Row,
+  Col,
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+import "../styles/ActivitiesSection.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 const ActivitiesSection = () => {
-    
-    const ActivitiesCard = (activity) => {
-        return (
-            <Col sm={{ size: 4 }} className="card-padding">
-                <Card className="achievement-card">
-                    <CardBody>
-                        <CardTitle>{activity.org}</CardTitle>
-                        <CardTitle>{activity.title}</CardTitle>
-                        <CardText>{activity.date}</CardText>
-                        <CardText>{activity.text}</CardText>
-                        <CardText>{activity.text2}</CardText>
-                    </CardBody>
-                </Card>
-            </Col>
-        );
-    }
-
+  const ActivitiesCard = (activity) => {
     return (
-        <div className="activities-section">
-            <div className="block-padding grey-section">
-                <Container>
-                    <Row className="row-padding">
-                        <Col sm={{ size: 6, offset: 3 }}>
-                            <div>
-                                <h2>Activities</h2>
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row className="row-padding">
-                    <ActivitiesCard
-                        title="Education Officer, Mentor"
-                        org="Association of Computer Machinery, UTD Chapter"
-                        date="August 2018 - Present"
-                        text="Hosting workshops on campus on topics of general computing as part of the Technical Workshop Divison"
-                        text2="Mentoring computer science freshmen in the development of academic, social and personal skills necessary to succeed in college"
-                    />
-                    <ActivitiesCard
-                        title="Event Planner"
-                        org="Women Mentoring Women in Engineering, UTD Chapter"
-                        date="May 2020 - Present"
-                        text="Responsible for planning events for groups of professional and academic mentors with undergraduate women enrolled in STEM"
-                        />
-                        <ActivitiesCard
-                        title="Technical Lead - Cloud"
-                        org="Google Developer Students Club, UTD Chapter"
-                        date="December 2019 - Present"
-                        text="Hosting workshops on campus on topics of Google Cloud Platform and backend developement"
-                        text2="Leading and managing teams of undergraduate and graduate students 
+      <Col sm={{ size: 4 }} className="card-padding">
+        <Card className="achievement-card">
+          <CardBody>
+            <CardTitle>{activity.org}</CardTitle>
+            <CardTitle>{activity.title}</CardTitle>
+            <CardText>{activity.date}</CardText>
+            <CardText>{activity.text}</CardText>
+            <CardText>{activity.text2}</CardText>
+            <CardText>{activity.text3}</CardText>
+          </CardBody>
+        </Card>
+      </Col>
+    );
+  };
+
+  return (
+    <div className="activities-section">
+      <div className="block-padding grey-section">
+        <Container>
+          <Row className="row-padding">
+            <Col sm={{ size: 6, offset: 3 }}>
+              <div>
+                <h2>Activities</h2>
+              </div>
+            </Col>
+          </Row>
+          <Row className="row-padding">
+            <ActivitiesCard
+              title="Education Officer, Developer, Mentor"
+              org="Association of Computer Machinery, UTD Chapter"
+              date="August 2018 - Present"
+              text="Hosting workshops on campus on topics of general computing as part of the Technical Workshop Divison"
+              text2="Mentoring computer science freshmen in the development of academic, social and personal skills necessary to succeed in college"
+            />
+            <ActivitiesCard
+              title="Event Planner"
+              org="Women Mentoring Women in Engineering, UTD Chapter"
+              date="May 2020 - Present"
+              text="Responsible for planning events for groups of professional and academic mentors with undergraduate women enrolled in STEM"
+            />
+            <ActivitiesCard
+              title="Technical Lead - Cloud"
+              org="Google Developer Students Club, UTD Chapter"
+              date="December 2019 - Present"
+              text="Hosting workshops on campus on topics of Google Cloud Platform and backend developement"
+              text2="Leading and managing teams of undergraduate and graduate students 
                         to develop projects in a team environment for the Google Solution Challenge 
                         while enforcing Agile software development"
-                        />
-                    </Row>
-                </Container>
-            </div>
-        </div>
-    );
+            />
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default ActivitiesSection;
