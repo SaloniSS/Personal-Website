2c73567ed2891b31097d0293d7f5dccbcbdcda49
SaloniSS <sxs170015@utdallas.edu>
2020-07-26 23:27:01 -0500

    path script testing

--- server/index.js
+++ server/index.js
@@ -22,6 +22,10 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, world!").end();
+});
+
 app.use("/api/v1/projects", projects);
 
 const PORT = process.env.PORT || 8000;
