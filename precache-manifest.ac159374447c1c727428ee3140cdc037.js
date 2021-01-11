6a3f521074af0e4fc5cfde74092897c357e1c76a
SaloniSS <sxs170015@utdallas.edu>
2021-01-10 20:03:46 -0600

    Updates

---
+++ precache-manifest.ac159374447c1c727428ee3140cdc037.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7451b8c08ca07a45b648bdf7d0cc0ff4",
+    "url": "/Personal-Website/index.html"
+  },
+  {
+    "revision": "863ba8cf88b484ac0f0f",
+    "url": "/Personal-Website/static/css/2.dc82ce2b.chunk.css"
+  },
+  {
+    "revision": "bb973989b97188a918de",
+    "url": "/Personal-Website/static/css/main.2208f72a.chunk.css"
+  },
+  {
+    "revision": "863ba8cf88b484ac0f0f",
+    "url": "/Personal-Website/static/js/2.95f4529b.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/Personal-Website/static/js/2.95f4529b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb973989b97188a918de",
+    "url": "/Personal-Website/static/js/main.bd23848c.chunk.js"
+  },
+  {
+    "revision": "349854b9753923afaf0c",
+    "url": "/Personal-Website/static/js/runtime-main.4d4c5a9a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Personal-Website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Personal-Website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Personal-Website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Personal-Website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Personal-Website/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
