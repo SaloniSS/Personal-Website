6c0e73e7f03a4ec047b817d8aac49148d14f11a6
SaloniSS <sxs170015@utdallas.edu>
2021-01-12 14:09:29 -0600

    dashboard

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,12 +1,15 @@
+//https://marmelab.com/react-admin/Tutorial.html
+
 import * as React from "react";
 import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
+import Dashboard from "./dashboard";
 import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
     <Resource
       name="posts"
       list={PostList}
