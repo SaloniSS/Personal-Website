2b6e6c29abedaa05f1788ab372a2e1a3aada79cb
SaloniSS <sxs170015@utdallas.edu>
2021-01-12 12:50:48 -0600

    oops 6

--- website/src/scripts/HeaderSection.js
+++ website/src/scripts/HeaderSection.js
@@ -11,7 +11,7 @@ const HeaderSection = (props) => {
       <h1> I am {name}! </h1>
       <Typed
         strings={[
-          "test5 Cyber Security Engineer",
+          "test6 Cyber Security Engineer",
           "Full Stack Developer",
           "CTF and Hackathon Enthusiast",
           "Software Engineer",
