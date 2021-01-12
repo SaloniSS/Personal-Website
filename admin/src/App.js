7b126f5c263d04481584f0bf479e9c4b8f181da3
SaloniSS <sxs170015@utdallas.edu>
2021-01-12 14:06:25 -0600

    stuff

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,8 +1,20 @@
 import * as React from "react";
-import { Admin } from "react-admin";
+import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
 import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+    />
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
 
 export default App;
