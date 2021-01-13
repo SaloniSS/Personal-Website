f8171b859a7cbb7c812f2d67bedfc2fe7da60373
SaloniSS <sxs170015@utdallas.edu>
2021-01-13 16:28:44 -0600

    used own api

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,36 +2,44 @@
 //https://github.com/spintech-software/react-admin-auth0-example
 
 import * as React from "react";
-import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
-import { PostList, PostEdit, PostCreate } from "./posts";
-import { UserList } from "./users";
+import { Admin, Resource } from "react-admin";
+import { ProjectList, ProjectCreate } from "./components/projects";
 import Dashboard from "./dashboard";
 import jsonServerProvider from "ra-data-json-server";
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./components/login";
 import LogoutButton from "./components/logout";
+import dataProvider from "./components/dataProvider";
 
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const dataProvider1 = jsonServerProvider(
+  "https://jsonplaceholder.typicode.com"
+);
 
 const App = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   return (
     <div>
-      {(!isAuthenticated || user.sub != process.env.REACT_APP_ACCOUNT) && (
+      {(!isAuthenticated || user.sub !== process.env.REACT_APP_ACCOUNT) && (
         <LoginButton />
       )}
 
       {isAuthenticated && user.sub === process.env.REACT_APP_ACCOUNT && (
         <div>
-          <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+          {/* <Admin dashboard={Dashboard} dataProvider={dataProvider1}>
             <Resource
               name="posts"
               list={PostList}
               edit={PostEdit}
               create={PostCreate}
             />
-            <Resource name="users" list={UserList} />
+          </Admin> */}
+          <Admin dataProvider={dataProvider}>
+            <Resource
+              name="projects"
+              list={ProjectList}
+              create={ProjectCreate}
+            />
           </Admin>
           <LogoutButton />
         </div>
---
+++ admin/src/components/projects.js
@@ -0,0 +1,57 @@
+import * as React from "react";
+import {
+  List,
+  Datagrid,
+  TextField,
+  EditButton,
+  Create,
+  SimpleForm,
+  TextInput,
+  UrlField,
+  BooleanField,
+  BooleanInput,
+  SelectInput,
+} from "react-admin";
+
+export const ProjectList = (props) => (
+  <List {...props}>
+    <Datagrid>
+      {/* <TextField source="id" /> */}
+      <TextField source="title" />
+      <TextField source="organization" />
+      <TextField source="date" />
+      <TextField source="award" />
+      <TextField source="description" />
+      <TextField source="contribution" />
+      <UrlField source="portfolio" />
+      <UrlField source="code" />
+      <TextField source="category" />
+      <BooleanField source="featured" />
+      <EditButton />
+    </Datagrid>
+  </List>
+);
+
+export const ProjectCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="title" />
+      <TextInput source="organization" />
+      <TextInput source="date" />
+      <TextInput source="award" />
+      <TextInput multiline source="description" />
+      <TextInput multiline source="contribution" />
+      <TextInput source="portfolio" />
+      <TextInput source="code" />
+      <SelectInput
+        source="category"
+        choices={[
+          { id: "regular", name: "Regular" },
+          { id: "winner", name: "Winner" },
+          { id: "long", name: "Long" },
+        ]}
+      />
+      <BooleanInput label="Featured" source="featured" />
+    </SimpleForm>
+  </Create>
+);
