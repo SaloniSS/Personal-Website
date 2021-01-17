658f4b20a1368af3394b3240ba7152b801122646
SaloniSS <sxs170015@utdallas.edu>
2021-01-17 01:13:02 -0600

    testing auth0 integration

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,6 +9,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./components/login";
 import LogoutButton from "./components/logout";
 import dataProvider from "./components/dataProvider";
+import authProvider from "./components/authProvider";
+import loginPage from "./pages/login";
 
 // const dataProvider1 = jsonServerProvider(
 //   "https://jsonplaceholder.typicode.com"
@@ -18,32 +20,13 @@ const App = () => {
   const { user, isAuthenticated } = useAuth0();
 
   return (
-    <div>
-      {(!isAuthenticated || user.sub !== process.env.REACT_APP_ACCOUNT) && (
-        <LoginButton />
-      )}
-
-      {isAuthenticated && user.sub === process.env.REACT_APP_ACCOUNT && (
-        <div>
-          {/* <Admin dashboard={Dashboard} dataProvider={dataProvider1}>
-            <Resource
-              name="posts"
-              list={PostList}
-              edit={PostEdit}
-              create={PostCreate}
-            />
-          </Admin> */}
-          <Admin dataProvider={dataProvider}>
-            <Resource
-              name="projects"
-              list={ProjectList}
-              create={ProjectCreate}
-            />
-          </Admin>
-          <LogoutButton />
-        </div>
-      )}
-    </div>
+    <Admin
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+      loginPage={loginPage}
+    >
+      <Resource name="projects" list={ProjectList} create={ProjectCreate} />
+    </Admin>
   );
 };
 
